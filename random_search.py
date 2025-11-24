67429dcedf9683697995da41f1f222bfe09deb3b
Alp Guneri <e.alp.guneri@gmail.com>
2025-11-24 18:35:55 -0500

    feat: random search only on delta

--- random_search.py
+++ random_search.py
@@ -15,17 +15,18 @@ DELTA_UPPER_BOUND = 1.0
 NUM_TRIALS = 200
 NUM_SIMULATIONS = 100
 
-profits_and_hyperparameters = []
-
 OUTPUT_FILE = 'random-search.txt'
+
+
 def sample_uniform(upper_bound, lower_bound):
     r = random.random()
     scaling_factor = upper_bound - lower_bound
     return r * scaling_factor + lower_bound
 
 
-if __name__ == "__main__":
-    for i in range(NUM_TRIALS):
+def random_search_all_params(num_trials=NUM_TRIALS, output_file=OUTPUT_FILE):
+    profits_and_hyperparameters = []
+    for i in range(num_trials):
         PMPD = []
         TERPD = []
         for j in range(10):
@@ -44,9 +45,33 @@ if __name__ == "__main__":
         profits = simulator.run_simulation(agents=test_agents, num_simulations=NUM_SIMULATIONS)
         agent_profit = profits[agent.name]
         profits_and_hyperparameters.append((agent_profit, PMPD, TERPD, delta))
-        print(f"Trial {i} completed successfully, average_profit: {agent_profit/NUM_SIMULATIONS}")
+        print(f"Trial {i} completed successfully, average_profit: {agent_profit / NUM_SIMULATIONS}")
 
     profits_and_hyperparameters = sorted(profits_and_hyperparameters, key=lambda x: -x[0])
-    with open(OUTPUT_FILE, 'w') as file:
+    with open(output_file, 'w') as file:
         for item in profits_and_hyperparameters:
             file.write(f"{item}\n")
+
+
+def random_search_delta(num_trials=NUM_TRIALS, num_simulations=NUM_SIMULATIONS, output_file=OUTPUT_FILE):
+    profits_and_hyperparameters = []
+    for i in range(num_trials):
+        delta = sample_uniform(DELTA_UPPER_BOUND, DELTA_LOWER_BOUND)
+        agent = MyNDaysNCampaignsAgent(delta=delta)
+        test_agents = [agent] + \
+                      [Tier1NDaysNCampaignsAgent(name=f"Random Agent {i + 1}") for i in range(6)] + \
+                      [Tier1NDaysNCampaignsAgent(name=f"Conservative Agent {i + 1}") for i in range(3)]
+
+        simulator = AdXGameSimulator()
+        profits = simulator.run_simulation(agents=test_agents, num_simulations=num_simulations)
+        agent_profit = profits[agent.name]
+        profits_and_hyperparameters.append((agent_profit, delta))
+        print(f"Trial {i} completed successfully, average_profit: {agent_profit / NUM_SIMULATIONS}")
+        profits_and_hyperparameters = sorted(profits_and_hyperparameters, key=lambda x: -x[0])
+        with open(output_file, 'w') as file:
+            for item in profits_and_hyperparameters:
+                file.write(f"{item}\n")
+
+
+if __name__ == "__main__":
+    random_search_delta()
